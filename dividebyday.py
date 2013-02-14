5e3d07072168862586a846c1e3259a2b69597a38
David Darmon <ddarmon@math.umd.edu>
2013-02-14 14:30:05 -0500

    Update dividebyday.py
    dividebyday now takes in a single timeseries and outputs a timeseries
    broken up by day, with the start and end of the day specified.

--- dividebyday.py
+++ dividebyday.py
@@ -1,9 +1,32 @@
 import datetime
 import numpy
 
+def is_same_day(day1, day2):
+	if (day1.day != day2.day):
+		is_same = False
+	elif (day1.month != day2.month):
+		is_same = False
+	elif (day1.year != day2.year):
+		is_same = False
+	else:
+		is_same = True
+
+	return is_same
+
+def is_in_window(day, starttime, endtime):
+	diff_back = (day - starttime).total_seconds()
+	diff_forward = (endtime - day).total_seconds()
+	if (diff_back < 0) or (diff_forward < 0):
+		is_in = False
+	else:
+		is_in = True
+
+	return is_in
+	
+
 numpy.random.seed(0) # Make the result reproducible
 
-x = numpy.random.randint(low = 1, high = 1000, size = 1000)
+x = numpy.random.randint(low = 1, high = 3000, size = 1000)
 
 increments = x.cumsum() # The number of seconds to add to reference_date
 
@@ -28,6 +51,32 @@ for increment in increments:
 starttime = datetime.datetime(year = reference_date.year, month = reference_date.month, day = reference_date.day, hour = 6)
 endtime   = datetime.datetime(year = reference_date.year, month = reference_date.month, day = reference_date.day, hour = 22)
 
+# ts_by_day is a list of list, where the internal lists contain the
+# timeseries broken up by day
+
+ts_by_day = [[]]
+
 for timepoint in ts:
-	if starttime:
-		pass #START HERE. Think this through, *carefully*. Use a picture!
+	# Check if we're still in the same day bracketed 
+	# by starttime and endtime.
+
+	if (is_same_day(timepoint, starttime)): # we're in the same day, so append if we're in the window
+		if (is_in_window(timepoint, starttime, endtime)): # We're in the desired window
+			ts_by_day[-1].append(timepoint)
+		else: # we're not in the desired window, so don't record it
+			pass
+	else: # we're not in the same day, so append a new day to ts_by_day
+		if len(ts_by_day[-1]) != 0:
+			ts_by_day.append([])
+
+		# Update our window to the current day
+
+		starttime = starttime.replace(day = timepoint.day, month = timepoint.month, year = timepoint.year)
+
+		endtime = endtime.replace(day = timepoint.day, month = timepoint.month, year = timepoint.year)
+
+		if (is_in_window(timepoint, starttime, endtime)): # We're in the desired window
+			ts_by_day[-1].append(timepoint)
+		else:
+			pass
+
