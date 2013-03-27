67c97078094fe742d95495d8a37ba47d40aaec81
David Darmon <ddarmon@math.umd.edu>
2013-03-27 10:55:19 -0400

    Modified open_files.py to take arguments from the command line.
    Takes arguments for what sort of ranking method to use and the desired
    rank.

--- open_files.py
+++ open_files.py
@@ -1,3 +1,13 @@
+# This file open the raster and .dot file representing
+# the epsilon-machine for a given user. Users can be
+# chosen based on their improvement rate over a 
+# coinflip predictor (order 0 epsilon-machine).
+#
+# Can run this script from the command line as:
+# 	open_files.py improvement_type improvement_rank
+#
+#	DMD, 270313-10-47
+
 import ipdb
 import cssr_interface
 import numpy
@@ -17,7 +27,13 @@ K = 1000
 
 users = get_K_users(K = K, start = rank_start)
 
-improvement_type = 'disimproved'
+if len(sys.argv) == 1:
+	ind = 0
+
+	improvement_type = 'improved'
+else:
+	improvement_type = sys.argv[1]
+	ind = int(sys.argv[2])
 
 # Import the uids of the improvement_type users, ranked from best->worst
 # (for 'improved') and worst->best (for 'disimproved')
@@ -28,10 +44,10 @@ uids = [int(line) for line in open('user_improvements/{}.txt'.format(improvement
 
 rate_lookup = numpy.loadtxt(fname = 'filtering_results.tsv', delimiter = '\t', skiprows = 1)
 
-ind = 0
 os.system('open rasters/raster-1s-{}.pdf'.format(users[uids[ind]]))
 os.system('open timeseries/byday-600s-{}-train.dat_inf.dot'.format(users[uids[ind]]))
 
 print 'For user ranked {} with userid {}...'.format(uids[ind], users[uids[ind]])
 print 'The baseline rate was: {}'.format(rate_lookup[uids[ind], 1])
 print 'The cm rate was: {}'.format(rate_lookup[uids[ind], 2])
+print 'The improvement in rate was: {}'.format(rate_lookup[uids[ind], 2] - rate_lookup[uids[ind], 1])
